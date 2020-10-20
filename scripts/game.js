e8a825cc14b90e04b26c930db1909be6848702d7
Samuel Denton <samdenton1995@hotmail.co.uk>
2020-10-20 12:29:49 +0000

    added in score and question counters

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 
 let currentQuestion = {};
 let acceptAnswers = false;
@@ -54,6 +56,8 @@ getNewQuestion = () => {
         return window.location.assign("/end.html");
     }
   questionCounter++;
+questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`; 
+
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
   question.innerText = currentQuestion.question;
@@ -80,7 +84,9 @@ const selectedAnswer = selectedChoice.dataset['number'];
 
 const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
-console.log(classToApply);
+if (classToApply === 'correct'){
+    incrementScore(CORRECT_BONUS);
+}
 
 selectedChoice.parentElement.classList.add(classToApply);
 
@@ -92,5 +98,10 @@ getNewQuestion();
 
     });
 });
+
+incrementScore = num =>{
+    score += num;
+    scoreText.innerText = score;
+}
 
 startGame();
